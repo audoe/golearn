db044343004fd61ad95c0eba4537f90c399d4c90
chenweihong <whchen@qq.com>
2022-07-24 21:47:46 +0800

    改进logging逻辑

--- main.go
+++ main.go
@@ -2,19 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"net/http"
 	"os"
 	"strings"
-	"time"
 )
-
-func hello(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	fmt.Fprintf(w, "hello\n")
-	Log(req)
-
-}
 
 // RemoteIP 通过 RemoteAddr 获取 IP 地址， 只是一个快速解析方法。
 func RemoteIP(r *http.Request) string {
@@ -24,33 +17,26 @@ func RemoteIP(r *http.Request) string {
 	return ""
 }
 
-func Log(r *http.Request) {
-	fmt.Println(time.Now().Format(time.RFC1123), "访问地址:", r.URL, RemoteIP(r))
-}
-
 func healthz(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "200\n")
-	Log(req)
+	fmt.Fprintf(w, "working\n")
+	log.Printf("%s %s %s", req.Method, req.RequestURI, RemoteIP(req))
 
 }
-func headers(w http.ResponseWriter, req *http.Request) {
-
+func index(w http.ResponseWriter, req *http.Request) {
 	for name, headers := range req.Header {
 		for _, h := range headers {
 			w.Header().Set(name, h)
 		}
 	}
-
+	os.Setenv("VERSION", "v0.0.1")
 	w.Header().Set("VERSION", os.Getenv("VERSION"))
-	fmt.Fprintf(w, "hello\n")
-	Log(req)
+	fmt.Fprintf(w, "headers\n")
+	log.Printf("%s %s %s", req.Method, req.RequestURI, RemoteIP(req))
 }
 
 func main() {
 
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/", index)
 	http.HandleFunc("/healthz", healthz)
-
 	http.ListenAndServe(":8090", nil)
 }
